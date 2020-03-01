1fcf744b9699a38344a241b5ea264035e503c45b
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-03-01 20:18:59 +0300

    inserted a table

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ $(document).ready(function(){
     });
     
 })
+const add = document.getElementById("add");
+
+add.addEventListener("click", function() {
+    const size = parseInt(document.getElementById("size").value);
+    const crust = parseInt(document.getElementById("crust").value);
+    const topping = parseInt(document.getElementById("top").value);
+    const quantity = parseInt(document.getElementById("num").value);
+
+    const results = function() {
+        const table = document.getElementById("table");
+        const row = table.insertRow();
+        const quantity1 = row.insertCell();
+        const size2 = row.insertCell();
+        const crust4 = row.insertCell();
+        const toppings3 = row.insertCell();
+        const total5 = row.insertCell();
+        quantity1.innerHTML = quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust;
+        toppings3.innerHTML = topping;
+        total5.innerHTML = (size + crust + topping) * quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust.options[crust.selectedIndex].text;
+        toppings3.innerHTML = topping;
+        quantity1.innerHTML = quantity.options[quantity.selectedIndex].text;
+
+    }
+    results();
+})
